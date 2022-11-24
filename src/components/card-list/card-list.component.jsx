f8642bb3cd49e0b870c168a978ca68f08ba19562
Fatykov Kirill <fatykovkirill@gmail.com>
2022-11-24 12:57:36 +0500

    creat project

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,16 @@
+import Card from "../card/card.component";
+
+import './card-list.style.css';
+
+const CardList = ({ monsters }) => (
+    <div className="card-list">
+        {monsters.map((monster) => {
+
+            return (
+                <Card key={monster.id} monster={monster} />
+            )
+        })}
+    </div>
+)
+
+export default CardList;
